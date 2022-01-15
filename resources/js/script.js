e1f1814cdd83819be3850758d67f850ec37b2abc
kwcortum <96136470+kwcortum@users.noreply.github.com>
2022-01-15 13:17:20 -0600

    Finished first function, started second gamePlay function

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,16 +1,21 @@
 function computerPlay() {
-    let randomNumber = Math.floor(Math.random()*3);
-    let computerChoice;
-    if (randomNumber % 3 === 0) {
-      computerChoice = "rock"
-    } else if (randomNumber % 3 === 1) {
-      computerChoice = "paper";
-    } else {
-      computerChoice = "scissors";
-      
-    }
+  let randomNumber = Math.floor(Math.random()*3);
+  let computerChoice;
+  if (randomNumber % 3 === 0) {
+    computerChoice = "rock"
     console.log(computerChoice);
-    return computerChoice;
+  } else if (randomNumber % 3 === 1) {
+    computerChoice = "paper";
+    console.log(computerChoice);
+  } else {
+    computerChoice = "scissors";
+    console.log(computerChoice);
+  }
+  return computerChoice;
+}
+
+function gamePlay(playerSelection, computerChoice) {
+
 }
 
 computerPlay();
